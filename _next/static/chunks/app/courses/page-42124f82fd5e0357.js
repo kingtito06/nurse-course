889c3d70bd0972a9c6625b103b7842c3671f2070
kingtito06 <149125296+kingtito06@users.noreply.github.com>
2025-08-14 16:14:15 -0500

    Updates

---
+++ _next/static/chunks/app/courses/page-42124f82fd5e0357.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[295],{1462:()=>{},1588:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6961,23)),Promise.resolve().then(n.t.bind(n,1462,23))},6961:()=>{}},e=>{e.O(0,[624,391,441,964,358],()=>e(e.s=1588)),_N_E=e.O()}]);
