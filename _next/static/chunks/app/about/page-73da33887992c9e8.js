5101c7ca74a63cce48b16cbced7c8f6efff3beaa
kingtito06 <149125296+kingtito06@users.noreply.github.com>
2025-08-14 16:43:39 -0500

    Updates

--- _next/static/chunks/app/about/page-73da33887992c9e8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{1988:()=>{},6882:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6961,23)),Promise.resolve().then(n.t.bind(n,1988,23))},6961:()=>{}},e=>{e.O(0,[624,573,441,964,358],()=>e(e.s=6882)),_N_E=e.O()}]);
