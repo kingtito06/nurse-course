0528ed5b3e8e2b7fe2e704ccfc16c21c1f92f043
kingtito06 <149125296+kingtito06@users.noreply.github.com>
2025-08-14 16:25:55 -0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/nurse-course',
+  output: 'export',       // enables static HTML export
   images: {
-    unoptimized: true,
+    unoptimized: true,    // allows images without server optimization
   },
 }
 
-export default nextConfig
+export default nextConfig;
