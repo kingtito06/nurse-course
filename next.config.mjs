35d57b2191e36343937b63228018435bd9f98df3
kingtito06 <149125296+kingtito06@users.noreply.github.com>
2025-08-14 16:23:33 -0500

    keeping mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/nurse-course',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
